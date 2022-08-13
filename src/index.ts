859f4bde3f576839b687b63dd8c7ea6d2306d369
Moustafa-Elmolla <elmolla1988@hotmail.com>
2022-08-13 22:42:15 +0200

    add middleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import express, {Application, Request, Response} from 'express'
+import express, {Application, Request, Response} from 'express';
+import morgan from 'morgan';
 
 
 const PORT = 3000;
+//create server
 const app: Application = express();
+// logger middelware
+app.use(morgan('common'));
+// middleware to parse incoming requests
+app.use(express.json());
 
+//add routing
 app.get('/', (req: Request, res: Response) => {
     res.json({
         message: 'Hello World',
+    });
+});
+
+app.post('/', (req: Request, res: Response) => {
+    res.json({
+        message: 'Hello World from post',
+        data: req.body,
     });
 });
 
