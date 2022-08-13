93d3420f0e38826870f7515a268093f3883c9919
Moustafa-Elmolla <elmolla1988@hotmail.com>
2022-08-13 18:07:07 +0200

    config server and add jasmine configuration

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.log("hello world moustafa")
+import express, {Application, Request, Response} from 'express'
 
+
+const PORT = 3000;
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({
+        message: 'Hello World',
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is starting at port:${PORT}`);
+});
+
+export default app;
